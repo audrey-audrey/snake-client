88ad6ec8825174b430d1d0b9be8c666e5460c393
audrey-audrey <audreyuu@gmail.com>
2020-11-04 23:15:32 +0000

    Fixed linting & added more notes

--- input.js
+++ input.js
@@ -6,7 +6,7 @@ let connection;
  * Specifically, so that we can handle user input via stdin
  */
 const setupInput = function(conn) {
-  // connection
+  // connection is now defined with the input object (see return from connect function in client.js)
   connection = conn;
 
   const stdin = process.stdin;
@@ -15,10 +15,10 @@ const setupInput = function(conn) {
   stdin.resume();
 
   // handle input with stdin
-  stdin.on('data', handleUserInput);
+  stdin.on('data', handleUserInput); // ==> We don't need to pass on a parameter to handleUserInput stdin.on('data',...) automatically passes on data as parameter
 
   // sending movement commands
-  // conn.on('data', (data) => handleUserInput(data, connection));
+  // conn.on('data', (data) => handleUserInput(data, connection)); => we don't need this
 
   return stdin;
 };
@@ -30,10 +30,12 @@ const handleUserInput = function(key) {
     process.exit();
   }
 
+  // 'w' for up etc....
   switch (key) {
   case 'w':
+    //  You don't need to write conn.on('connection', .....) because a connection has already
+    // been established, it wont establish another connection so it wont execute
     connection.write('Move: up');
-    //  console.log('up');
     break;
   case 'a':
     connection.write('Move: left');
--- play.js
+++ play.js
@@ -5,7 +5,7 @@ const {setupInput} = require('./input');
 
 console.log('Connecting ...');
 
-// connect(); => is this needed?
+// connect(); => is this needed? No, we only need to pass a connection to setupInput
 
 // returned object from connect() as parameter in setupInput
 setupInput(connect());
